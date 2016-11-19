b4efaa486adc19bccb1f513a207da55eb7c25fa4
Jaime Martinez <jmslbam@gmail.com>
2016-11-19 12:42:45 +0100

    Commit first pass at Travis and PHPUnit test

---
+++ bin/travis.before_script.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+shopt -s expand_aliases
+
+# TODO: These should not override any existing environment variables
+export WP_CORE_DIR=/tmp/wordpress
+export WP_TESTS_DIR=${WP_CORE_DIR}/tests/phpunit
+export PLUGIN_DIR=$(pwd)
+export PLUGIN_SLUG=$(basename $(pwd) | sed 's/^wp-//')
+export PHPCS_DIR=/tmp/phpcs
+export PHPCS_GITHUB_SRC=squizlabs/PHP_CodeSniffer
+export PHPCS_GIT_TREE=master
+export PHPCS_IGNORE='tests/*,vendor/*,binb/*'
+export WPCS_DIR=/tmp/wpcs
+export WPCS_GITHUB_SRC=WordPress-Coding-Standards/WordPress-Coding-Standards
+export WPCS_GIT_TREE=master
+export DISALLOW_EXECUTE_BIT=0
+export LIMIT_TRAVIS_PR_CHECK_SCOPE=files # when set to 'patches', limits reports to only lines changed; TRAVIS_PULL_REQUEST must not be 'false'
+export PATH_INCLUDES=./
+export WPCS_STANDARD=$(if [ -e phpcs.ruleset.xml ]; then echo phpcs.ruleset.xml; else echo WordPress-Core; fi)
+
+
+# Load a .ci-env.sh to override the above environment variables
+if [ -e .ci-env.sh ]; then
+	source .ci-env.sh
+fi
+
+# Install the WordPress Unit Tests
+if [ -e phpunit.xml ] || [ -e phpunit.xml.dist ]; then
+	bash bin/install-wp-tests.sh wordpress_test root root localhost $WP_VERSION
+	cd ${WP_CORE_DIR}/wp-content/plugins
+	mv $PLUGIN_DIR $PLUGIN_SLUG
+	cd $PLUGIN_SLUG
+	ln -s $(pwd) $PLUGIN_DIR
+	echo "Plugin location: $(pwd)"
+
+	if ! command -v phpunit >/dev/null 2>&1; then
+		wget -O /tmp/phpunit.phar https://phar.phpunit.de/phpunit.phar
+		chmod +x /tmp/phpunit.phar
+		alias phpunit='/tmp/phpunit.phar'
+	fi
+fi
+
+# Install PHP_CodeSniffer and the WordPress Coding Standards
+mkdir -p $PHPCS_DIR && curl -L https://github.com/$PHPCS_GITHUB_SRC/archive/$PHPCS_GIT_TREE.tar.gz | tar xz --strip-components=1 -C $PHPCS_DIR
+mkdir -p $WPCS_DIR && curl -L https://github.com/$WPCS_GITHUB_SRC/archive/$WPCS_GIT_TREE.tar.gz | tar xz --strip-components=1 -C $WPCS_DIR
+$PHPCS_DIR/scripts/phpcs --config-set installed_paths $WPCS_DIR
+
+# Install Composer
+if [ -e composer.json ]; then
+	curl -s http://getcomposer.org/installer | php && php composer.phar install
+fi
+
+set +e
---
+++ bin/travis.script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+if [ "$TRAVIS_PULL_REQUEST" != 'false' ] && ( [ "$LIMIT_TRAVIS_PR_CHECK_SCOPE" == 'files' ] || [ "$LIMIT_TRAVIS_PR_CHECK_SCOPE" == 'patches' ] ); then
+	git diff --diff-filter=AM --no-prefix --unified=0 $TRAVIS_BRANCH...$TRAVIS_COMMIT -- $PATH_INCLUDES | php bin/parse-diff-ranges.php  > /tmp/checked-files
+else
+	find $PATH_INCLUDES -type f | grep -v -E "^./(bin|\.git)/" | sed 's:^\.//*::' > /tmp/checked-files
+fi
+
+echo "LIMIT_TRAVIS_PR_CHECK_SCOPE: $LIMIT_TRAVIS_PR_CHECK_SCOPE"
+echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo "Files to check:"
+cat /tmp/checked-files
+echo
+
+function remove_diff_range {
+	sed 's/:[0-9][0-9]*-[0-9][0-9]*$//' | sort | uniq
+}
+function filter_php_files {
+	 grep -E '\.php(:|$)'
+}
+function filter_js_files {
+	grep -E '\.js(:|$)'
+}
+
+# Run PHP syntax check
+for php_file in $( cat /tmp/checked-files | remove_diff_range | filter_php_files ); do
+	php -lf "$php_file"
+done
+
+
+# Run PHP_CodeSniffer
+echo "## PHP_CodeSniffer"
+if ! cat /tmp/checked-files | remove_diff_range | filter_php_files | xargs --no-run-if-empty $PHPCS_DIR/scripts/phpcs -s --report-emacs=/tmp/phpcs-report --standard=$WPCS_STANDARD $(if [ -n "$PHPCS_IGNORE" ]; then echo --ignore=$PHPCS_IGNORE; fi); then
+    cat /tmp/phpcs-report
+    exit 1
+fi
+
+# Run PHPUnit tests
+if [ -e phpunit.xml ] || [ -e phpunit.xml.dist ]; then
+	phpunit $( if [ -e .coveralls.yml ]; then echo --coverage-clover build/logs/clover.xml; fi )
+fi
