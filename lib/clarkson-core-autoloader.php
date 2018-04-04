23c53e2386169f103cd6af1c74f9ca61d25cc1f3
Jaime Martinez <jmslbam@gmail.com>
2018-04-04 21:46:16 +0200

    Load custom objects for custom templates

--- lib/clarkson-core-autoloader.php
+++ lib/clarkson-core-autoloader.php
@@ -4,14 +4,16 @@ class Clarkson_Core_Autoloader {
 	public $post_types = array();
 	public $taxonomies = array();
 	public $user_types = array();
+	public $extra 	   = array();
 
 	public function __construct() {
-		spl_autoload_register( array( $this, 'load_wordpress_object' ), true, true );
 		add_action( 'registered_post_type', array( $this, 'registered_post_type' ), 10, 1 );
 		add_action( 'registered_taxonomy', array( $this, 'registered_taxonomy' ), 10, 1 );
+		add_action( 'parse_query', array( $this, 'load_template_objects' ) ); // could also run on 'wp' but this is already 
+		spl_autoload_register( array( $this, 'load_wordpress_objects' ), true, true );
 
 		$filepath = realpath( get_template_directory() ) . '/wordpress-objects/User.php';
-		if (file_exists( $filepath )) {
+		if ( file_exists( $filepath ) ) {
 			$this->user_types['user'] = 'user';
 		}
 	}
@@ -35,28 +37,58 @@ class Clarkson_Core_Autoloader {
 	}
 
 	/**
-	 * Fill $post_type variable with all registered CPT's
+	 * Fill $post_type variable with all registered CPT's,
+	 * every time a post type is registered via WordPress after the "registered_post_type" action.
 	 *
 	 * This also means reserved ones like page, post, attachment, revision, nav_menu_item,
 	 * custom_css and customize_changeset.
 	 */
-	public function registered_post_type( $post_type) {
+	public function registered_post_type( $post_type ) {
 		$this->post_types[ $this->sanitize_object_name( $post_type ) ] = $this->sanitize_object_name( $post_type );
 	}
 
 	/**
-	 * Fill $taxonomies variable with all registered Taxonomies
+	 * Fill $taxonomies variable with all registered Taxonomies,
+	 * every time a taxonomy is registered via WordPress after the "registered_taxonomy" action.
 	 */
-	public function registered_taxonomy( $taxonomy) {
+	public function registered_taxonomy( $taxonomy ) {
 		$this->taxonomies[ $this->sanitize_object_name( $taxonomy ) ] = $this->sanitize_object_name( $taxonomy );
 	}
 
-	protected function load_wordpress_object( $classname) {
+	/**
+	 *  Returns page template filename without extension. Returns an empty string when the default page template is in use. Returns false if the post is not a page.
+	 */
+	public function get_template_filename( $post_id ){
+		$page_template_slug = get_page_template_slug( $post_id );
+		$filename = '';
+
+		if( !empty( $page_template_slug ) ){
+			$pathinfo = pathinfo( $page_template_slug );
+			$filename = array_key_exists( 'filename', $pathinfo ) ? $pathinfo['filename'] : '';		
+		}
+
+		return $filename;
+	}
+
+	/**
+	 *  Fill $extra variable with the current custom template
+	 */
+	public function load_template_objects(){
+		$template_name = $this->get_template_filename( get_queried_object_id() );
+		
+		if( $template_name && !empty( $template_name ) ){
+			$template_name = $this->sanitize_object_name( $template_name );
+			$this->extra[ $template_name ] = $template_name;
+		}
+	}
+
+	protected function load_wordpress_objects( $classname ) {
 		$type = $this->sanitize_object_name( $classname );
 
-		if ( ! in_array( $type, $this->post_types ) && ! in_array( $type, $this->taxonomies )) {
+		if ( ! in_array( $type, $this->post_types ) && ! in_array( $type, $this->taxonomies ) && ! in_array( $type, $this->extra ) ) {
 			return;
 		}
+		
 		$filename = "{$classname}.php";
 
 		// Load child theme first
