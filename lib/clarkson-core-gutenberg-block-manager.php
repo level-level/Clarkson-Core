0c391877b2140ee9ae2294eca845222995cd9a69
Niels de Blaauw <niels@level-level.com>
2019-05-16 10:23:59 +0200

    Adds documentation and filters

--- lib/clarkson-core-gutenberg-block-manager.php
+++ lib/clarkson-core-gutenberg-block-manager.php
@@ -1,18 +1,36 @@
 <?php
 
+/**
+ * Intercepts 'the content' filter to allow overriding of the rendering functions
+ * of Gutenberg blocks. This allows us to use twig for block rendering.
+ */
 class Clarkson_Core_Gutenberg_Block_Manager {
+	/**
+	 * Hook in as soon as we can, to replace blocks with the Clarkson-block equivalent.
+	 */
 	public function init() {
 		add_filter( 'the_content', array( $this, 'intercept_gutenberg_rendering' ), 1 );
 	}
 
+	/**
+	 * Attempts to create a custom block from class from a block. Falls back
+	 * to a default Clarkson Core block type.
+	 * @return string
+	 */
 	protected function determine_block_type_class( $block_type ) {
 		$class_name = '\\Gutenberg\\Blocks\\' . $this->sanitize_block_type_name( $block_type->name );
+		$class_name = apply_filters( 'clarkson_core_gutenberg_block_class', $class_name, $block_type );
+		$class_name = apply_filters( 'clarkson_core_gutenberg_block_class_' . $block_type->name, $class_name, $block_type );
 		if ( class_exists( $class_name ) ) {
 			return $class_name;
 		}
 		return '\Clarkson_Core_Gutenberg_Block_Type';
 	}
 
+	/**
+	 * Filters a block name into a valid class name.
+	 * @return string
+	 */
 	protected function sanitize_block_type_name( $str ) {
 		$str = trim( $str );
 
@@ -28,6 +46,14 @@ class Clarkson_Core_Gutenberg_Block_Manager {
 		return $str;
 	}
 
+	/**
+	 * Replaces all registered non-core blocks with our intermediate class, so we
+	 * can manipulate the rendering function.
+	 *
+	 * Does not manipulate the $content.
+	 *
+	 * @return string
+	 */
 	public function intercept_gutenberg_rendering( $content ) {
 		$block_registry = \WP_Block_Type_Registry::get_instance();
 		foreach ( $block_registry->get_all_registered() as $original_block ) {
--- lib/clarkson-core-gutenberg-block-type.php
+++ lib/clarkson-core-gutenberg-block-type.php
@@ -1,16 +1,33 @@
 <?php
 
+/**
+ * This custom block overwrites the render callback to use twig files.
+ * It is automatically injected into all registered blocks at render time.
+ */
 class Clarkson_Core_Gutenberg_Block_Type extends \WP_Block_Type {
+	/**
+	 * Replaces the original block render function, and saves the original render
+	 * function in case we can't find a fitting twig file.
+	 */
 	public function __construct( $block_type, $args = array() ) {
 		parent::__construct( $block_type, $args );
 		$this->original_render_callback = $this->render_callback;
 		$this->render_callback          = array( $this, 'clarkson_render_callback' );
 	}
 
+	/**
+	 * Allows filtering of the path where this blocks twig file is found.
+	 *
+	 * @return string
+	 */
 	public function get_twig_template_path() {
-		return get_stylesheet_directory() . '/templates/gutenberg/blocks/' . $this->name . '.twig';
+		return apply_filters( 'clarkson_core_gutenberg_block_template_' . $this->name, get_stylesheet_directory() . '/templates/partials/blocks/gb/' . $this->name . '.twig', $this );
 	}
 
+	/**
+	 * Tries to find a twig file to use for rendering. If the twig file doesn't
+	 * exists it falls back to the original render callback.
+	 */
 	public function clarkson_render_callback( $attributes, $content ) {
 		if ( file_exists( $this->get_twig_template_path() ) ) {
 			$cc_template              = Clarkson_Core_Templates::get_instance();
