be32f8664f6dfcfbfdd28c5e1abd630ccab7141f
Niels de Blaauw <niels@level-level.com>
2019-05-16 09:51:31 +0200

    Fixes code style for gutenberg branche

--- lib/clarkson-core-gutenberg-block-manager.php
+++ lib/clarkson-core-gutenberg-block-manager.php
@@ -1,19 +1,19 @@
 <?php
 
-class Clarkson_Core_Gutenberg_Block_Manager{
-	public function init(){
-		add_filter( 'the_content', array($this, 'intercept_gutenberg_rendering'), 1 );
+class Clarkson_Core_Gutenberg_Block_Manager {
+	public function init() {
+		add_filter( 'the_content', array( $this, 'intercept_gutenberg_rendering' ), 1 );
 	}
 
-	protected function determine_block_type_class($block_type){
-		$class_name = '\\Gutenberg\\Blocks\\' . $this->sanitize_block_type_name($block_type->name);
-		if(class_exists($class_name)){
+	protected function determine_block_type_class( $block_type ) {
+		$class_name = '\\Gutenberg\\Blocks\\' . $this->sanitize_block_type_name( $block_type->name );
+		if ( class_exists( $class_name ) ) {
 			return $class_name;
 		}
 		return '\Clarkson_Core_Gutenberg_Block_Type';
 	}
 
-	protected function sanitize_block_type_name($str){
+	protected function sanitize_block_type_name( $str ) {
 		$str = trim( $str );
 
 		// Replace - with _ .
@@ -28,13 +28,13 @@ class Clarkson_Core_Gutenberg_Block_Manager{
 		return $str;
 	}
 
-	public function intercept_gutenberg_rendering($content){
+	public function intercept_gutenberg_rendering( $content ) {
 		$block_registry = \WP_Block_Type_Registry::get_instance();
-		foreach($block_registry->get_all_registered() as $original_block){
-			$block_type = $this->determine_block_type_class($original_block);
-			$clarkson_block = new $block_type($original_block->name, get_object_vars( $original_block ));
-			$block_registry->unregister($original_block);
-			$block_registry->register($clarkson_block);
+		foreach ( $block_registry->get_all_registered() as $original_block ) {
+			$block_type     = $this->determine_block_type_class( $original_block );
+			$clarkson_block = new $block_type( $original_block->name, get_object_vars( $original_block ) );
+			$block_registry->unregister( $original_block );
+			$block_registry->register( $clarkson_block );
 		}
 		return $content;
 	}
