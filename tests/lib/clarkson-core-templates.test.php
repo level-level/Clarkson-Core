b88bcca71a87ea24f73ce47a9bab84a8842b9b96
Niels de Blaauw <niels@level-level.com>
2019-06-10 14:06:28 +0200

    Adds basic case for add_template filter

--- tests/lib/clarkson-core-templates.test.php
+++ tests/lib/clarkson-core-templates.test.php
@@ -15,5 +15,19 @@ class ClarksonCoreTemplatesTest extends \WP_Mock\Tools\TestCase {
 		\WP_Mock::userFunction( 'get_bloginfo' )->with( 'version' )->andReturn( '4.7' );
 		$cc_templates = \Clarkson_Core_Templates::get_instance();
 		$this->assertInstanceOf( \Clarkson_Core_Templates::class, $cc_templates );
+		return $cc_templates;
+	}
+
+	/**
+	 * @depends test_can_get_instance
+	 */
+	public function test_can_add_template( $cc_templates ) {
+		$term          = Mockery::mock( '\WP_Term' );
+		$term->taxonomy = 'category';
+		\WP_Mock::userFunction( 'get_queried_object')->andReturn($term );
+		\WP_Mock::userFunction( 'current_filter', 'category_template');
+		\WP_Mock::userFunction( 'get_post_type', null);
+		$this->assertEquals('template.twig', $cc_templates->add_template( 'template.twig' ));
+
 	}
 }
