9e5fe728c575ff92a9b4e3d12174f9ca35e73241
Niels de Blaauw <niels@level-level.com>
2019-06-11 17:29:01 +0200

    Includes clarkson-core main file to define the main class

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,15 @@
 <?php
 
-// First we need to load the composer autoloader so we can use WP Mock
+// First we need to load the composer autoloader so we can use WP Mock.
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Now call the bootstrap method of WP Mock
+// Now call the bootstrap method of WP Mock.
 WP_Mock::bootstrap();
+
+/**
+ * We need clarkson-core to define the main class, as it's not autoloaded.
+ */
+require_once __DIR__ . '/../clarkson-core.php';
 
 /**
  * Needs to be initialised for the unit test, otherwise it fails on a
