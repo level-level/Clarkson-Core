9ee6e16f91de6b737de8d0bfec273382b8f09f6b
Menno van den Ende <50165380+menno-ll@users.noreply.github.com>
2026-09-02 13:47:45 +0200

    add twig cache

--- lib/clarkson-core-templates.php
+++ lib/clarkson-core-templates.php
@@ -139,6 +139,11 @@ class Clarkson_Core_Templates {
 				'debug' => $debug,
 			);
 
+			if ( wp_get_environment_type() === 'production' ) {
+				$version            = wp_get_theme()->get( 'Version' ) ?: 'no-version';
+				$twig_args['cache'] = sys_get_temp_dir() . '/cache/twig/' . $version . '/';
+			}
+
 			/**
 			 * Allows manipulation of the twig envirionment settings.
 			 *
