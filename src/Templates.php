65023062aa7ba6e78fec22f20ac0c6da40aa18c2
Menno van den Ende <50165380+menno-ll@users.noreply.github.com>
2026-09-15 15:40:21 +0200

    fix twig cache location, include user

--- src/Templates.php
+++ src/Templates.php
@@ -130,7 +130,12 @@ class Templates {
 
 			if ( wp_get_environment_type() === 'production' ) {
 				$version            = wp_get_theme()->get( 'Version' ) ?: 'no-version';
-				$twig_args['cache'] = sys_get_temp_dir() . '/cache/twig/' . $version . '/';
+				$twig_args['cache'] = sprintf(
+					'%s/%s/cache/twig/%s/',
+					sys_get_temp_dir(),
+					get_current_user(),
+					$version
+				);
 			}
 			/**
 			 * Allows manipulation of the twig envirionment settings.
